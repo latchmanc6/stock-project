879aa0981a132cad9e44035c416c91209241dc1b
Nari Roh <nari.rohh@gmail.com>
2022-01-17 11:11:38 -0500

    feat: create user login with jwt

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { UsersModel } = require("../models");
 const bcrypt = require("bcrypt");
+const { sign } = require("jsonwebtoken");
 
 router.post("/", async (req, res) => {
   const { email, password } = req.body;
@@ -13,6 +14,36 @@ router.post("/", async (req, res) => {
     });
     res.json("Success");
   });
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await UsersModel.findOne({
+    where: { email },
+  });
+
+  if (!user) {
+    res.json({ error: "User doesn't exist" });
+    return;
+  }
+
+  bcrypt
+    .compare(password, user.password)
+    .then((match) => {
+      if (!match) {
+        res.json({ error: "Wrong email and password entered" });
+
+      } else {
+        const accessToken = sign(
+          { email: user.email, id: user.id },
+          "importantsecret"
+        );
+        res.json({ token: accessToken, email, id: user.id });
+      }
+    })
+    .catch((error) => {
+      res.json({ error: error.messages });
+    });
 });
 
 module.exports = router;
