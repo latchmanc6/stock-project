a6a4ab1802b12c72125f8e942043d8cc26b0bc22
latchmanc6 <latchmanc6@gmail.com>
2022-01-18 20:03:32 -0500

    feat: changed structure of chart page a bit, still needs work to be efficient

--- client/src/pages/components/StockChart.js
+++ client/src/pages/components/StockChart.js
@@ -25,59 +25,38 @@ ChartJS.register(
 
 function StockChart() {
   let { ticker } = useParams();
-  const [stockChartData, setStockChartData] = useState([]);
+  const [stockChartData, setStockChartData] = useState();
+  const [data, setData] = useState({ labels: [], datasets: [] });
 
   const getChartDataFromAPI = async () => {
-    const { data } = await axios.get(
-      `http://localhost:3001/api/stock/getStockChartData/${ticker}`
-    );
-    setStockChartData(data);
+    await axios
+      .get(`http://localhost:3001/api/stock/getStockChartData/${ticker}`)
+      .then((response) => {
+        console.log(response.data);
+        setStockChartData(response.data);
+      })
+      .then(() => {
+        console.log("Right before data is set for the chart");
+        setData({
+          labels: getDaysOfStockDataLabel(7),
+          datasets: [
+            {
+              label: `${ticker}`,
+              data: getDaysOfStockData(7),
+              borderColor: "rgb(255, 99, 132)",
+              backgroundColor: "rgba(255, 99, 132, 0.5)",
+            },
+          ],
+        });
+      })
+      .then(() => {
+        // Line.forceUpdate();
+      });
   };
 
-  const lineData1Week = {
-    labels: getDaysOfStockDataLabel(1000),
-    datasets: [
-      {
-        label: `${ticker}`,
-        data: getDaysOfStockData(1000),
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-    ],
-  };
-
-  const lineData1Month = {
-    labels: getDaysOfStockDataLabel(31),
-    datasets: [
-      {
-        label: `${ticker}`,
-        data: getDaysOfStockData(31),
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-    ],
-  };
-
-  const lineData3Months = {
-    labels: getDaysOfStockDataLabel(91),
-    datasets: [
-      {
-        label: `${ticker}`,
-        data: getDaysOfStockData(91),
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-    ],
-  };
-
-  function getDaysOfStockData(days) {
+  const getDaysOfStockData = (days) => {
     let data = [];
-    if (stockChartData.length !== 0) {
-    if (days >= stockChartData.length) {
-      days = stockChartData.length;
-    }
-    console.log("Days: " + days);
-    console.log("Stock Chart: " + stockChartData.length);
+    if (stockChartData !== undefined) {
       for (let i = 0; i < days; i++) {
         if (Object.keys(stockChartData)[i] === undefined) {
           return data;
@@ -86,25 +65,99 @@ function StockChart() {
       }
     }
     return data.reverse();
-  }
+  };
 
-  function getDaysOfStockDataLabel(days) {
+  const getDaysOfStockDataLabel = (days) => {
     let data = [];
-    if (stockChartData.length !== 0) {
-      if (days >= stockChartData.length) {
-        days = stockChartData.length;
-      }
-      console.log("Days Label: " + days);
-      console.log("Stock Chart Label: " + stockChartData.length);
+    if (stockChartData !== undefined) {
       for (let i = 0; i < days; i++) {
         if (Object.keys(stockChartData)[i] === null) {
           return data;
         }
+        console.log(days);
         data.push(Object.keys(stockChartData)[i]);
       }
     }
     return data.reverse();
-  }
+  };
+
+  const reRenderChart = () => {
+    this.forceUpdate();
+  };
+
+  // const lineData1Week = {
+  //   labels: getDaysOfStockDataLabel(7),
+  //   datasets: [
+  //     {
+  //       label: `${ticker}`,
+  //       data: getDaysOfStockData(7),
+  //       borderColor: "rgb(255, 99, 132)",
+  //       backgroundColor: "rgba(255, 99, 132, 0.5)",
+  //     },
+  //   ],
+  // };
+
+  // const lineData1Month = {
+  //   labels: getDaysOfStockDataLabel(31),
+  //   datasets: [
+  //     {
+  //       label: `${ticker}`,
+  //       data: getDaysOfStockData(31),
+  //       borderColor: "rgb(255, 99, 132)",
+  //       backgroundColor: "rgba(255, 99, 132, 0.5)",
+  //     },
+  //   ],
+  // };
+
+  // const lineData3Months = {
+  //   labels: getDaysOfStockDataLabel(91),
+  //   datasets: [
+  //     {
+  //       label: `${ticker}`,
+  //       data: getDaysOfStockData(91),
+  //       borderColor: "rgb(255, 99, 132)",
+  //       backgroundColor: "rgba(255, 99, 132, 0.5)",
+  //     },
+  //   ],
+  // };
+
+  // const lineData6Months = {
+  //   labels: getDaysOfStockDataLabel(182),
+  //   datasets: [
+  //     {
+  //       label: `${ticker}`,
+  //       data: getDaysOfStockData(182),
+  //       borderColor: "rgb(255, 99, 132)",
+  //       backgroundColor: "rgba(255, 99, 132, 0.5)",
+  //     },
+  //   ],
+  // };
+
+  // const lineData1Year = {
+  //   labels: getDaysOfStockDataLabel(365),
+  //   datasets: [
+  //     {
+  //       label: `${ticker}`,
+  //       data: getDaysOfStockData(365),
+  //       borderColor: "rgb(255, 99, 132)",
+  //       backgroundColor: "rgba(255, 99, 132, 0.5)",
+  //     },
+  //   ],
+  // };
+
+  // const lineDataMax = {
+  //   labels: getDaysOfStockDataLabel(20000),
+  //   datasets: [
+  //     {
+  //       label: `${ticker}`,
+  //       data: getDaysOfStockData(20000),
+  //       borderColor: "rgb(255, 99, 132)",
+  //       backgroundColor: "rgba(255, 99, 132, 0.5)",
+  //     },
+  //   ],
+  // };
+
+  const load1MChartData = () => {};
 
   useEffect(() => {
     getChartDataFromAPI();
@@ -112,7 +165,13 @@ function StockChart() {
 
   return (
     <div>
-      <Line data={lineData1Week} />
+      <Line data={data} onClick={reRenderChart} />
+      <button>1W</button>
+      <button onClick={load1MChartData}>1M</button>
+      <button>3M</button>
+      <button>6M</button>
+      <button>1Y</button>
+      <button>MAX</button>
     </div>
   );
 }
