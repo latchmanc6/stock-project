24d94c3ec9ffe9af8e6b0feea3aa128905c0aefb
Nari Roh <nari.rohh@gmail.com>
2022-01-21 21:17:53 -0500

    feat: implement GET stockList route

--- server/routes/portfolioRoute.js
+++ server/routes/portfolioRoute.js
@@ -1,19 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const { Users } = require("../models");
+const { Users, StockTransactions, Stocks } = require("../models");
 const { validateToken } = require("../middlewares/AuthMiddleware");
 
 router.get("/", validateToken, async (req, res) => {
   const user = await Users.findByPk(req.user.id);
 
   if (!user) {
-    res.json({error: "user dosen't exist"});
+    res.json({ error: "user dosen't exist" });
   } else {
-    res.json(user)
-
+    res.json(user);
   }
+});
 
-  // res.json(user)
+router.get("/stockList", validateToken, async (req, res) => {
+  const { id } = req.user;
+
+
+  const stockList = await StockTransactions.findAll({
+    where: { UserId: id }, 
+    include: [{
+      model: Stocks,
+      as: "Stocks",
+    }]
+  });
+  res.json(stockList);
 });
 
 module.exports = router;
