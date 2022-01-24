fc17147350015d4f1d2c2934018e3d0ca5dc0361
Nari Roh <nari.rohh@gmail.com>
2022-01-24 00:42:44 -0500

    feat: display data on portfolio page & styling table

--- client/src/components/Styled/style.js
+++ client/src/components/Styled/style.js
@@ -1,40 +1,51 @@
 import styled from "styled-components";
-import * as StyledButton from './StyledButton'
+import * as StyledButton from "./StyledButton";
 
 /*--------- Button design ----------*/
 
-/** 
+/**
  * variants='primary = dark green', 'secondary = yellow'
- * 
+ *
  * import { Button } from 'components/Styled/style.js';
  * <Button variant='primary | secondary'/>
-*/
+ */
 
 const ButtonBase = styled.button`
   ${StyledButton.buttonbase};
-  ${props => StyledButton[props.variant]};
+  ${(props) => StyledButton[props.variant]};
 `;
 
 export const Button = ({ variant, ...rest }) => (
-  <ButtonBase
-    variant={variant}
-    {...rest}
-  />
+  <ButtonBase variant={variant} {...rest} />
 );
 
-/** 
+/**
  * override Bootstrap <Card> component design
- * 
+ *
  * import { CardRound } from 'components/Styled/style.js';
-*/
+ */
 
 export const CardRound = styled.div.attrs({
-  className: "card"
+  className: "card",
 })`
   border-radius: 10px;
   background-color: rgb(255, 255, 255);
   box-shadow: rgb(59 59 59 / 5%) 0px 5px 15px 0px;
   font-size: 16px;
   border: none;
-`
-// 
+`;
+
+/**
+ * override Bootstrap <Table> component design
+ *
+ * import { TableRound } from 'components/Styled/style.js';
+ */
+
+export const TableRound = styled.table.attrs({
+  clasaName: "table",
+})`
+  border-radius: 10px;
+  background-color: rgb(255,255,255);
+  box-shadow: rgb(59 59 59 / 5%) 0px 5px 15px 0px;
+  width: 100%;
+`;
--- server/routes/portfolioRoute.js
+++ server/routes/portfolioRoute.js
@@ -8,6 +8,27 @@ const fetch = require("node-fetch");
 const { response } = require("express");
 require("dotenv").config();
 
+// const stockList = await StockTransactions.findAll({
+//   where: { UserId },
+//   attributes: [
+//     "ticker",
+//     [
+//       Sequelize.literal(
+//         "SUM(CASE type WHEN 'buy' THEN quantity else 0 end) - SUM(CASE type WHEN 'sell' THEN quantity else 0 end)"
+//       ),
+//       `totalCounts`,
+//     ],
+//     [Sequelize.literal("SUM(CASE type WHEN 'buy' THEN total else 0 end)"),`buyTotal`],
+//     [Sequelize.literal("buyTotal / SUM(CASE type WHEN 'buy' THEN quantity else 0 end)"),`buyAvg`],
+//   ],
+//   include: [{
+//     model: Stocks,
+//     as: "Stocks",
+//     attributes: ['id', 'companyName', 'currentPrice', 'exchange', 'updatedAt'],
+//   }],
+//   group: ["ticker", "Stocks.ticker"],
+// });
+
 const FinnhubAPIKey = process.env.FINNHUB_API_KEY;
 
 router.get("/stockList", validateToken, async (req, res) => {
@@ -24,36 +45,56 @@ router.get("/stockList", validateToken, async (req, res) => {
         ),
         `totalCounts`,
       ],
+      [
+        Sequelize.literal("SUM(CASE type WHEN 'buy' THEN total else 0 end)"),
+        `buyTotal`,
+      ],
+      [
+        Sequelize.literal(
+          "SUM(CASE type WHEN 'buy' THEN total else 0 end) / SUM(CASE type WHEN 'buy' THEN quantity else 0 end)"
+        ),
+        `buyAvg`,
+      ],
     ],
-    include: [{
-      model: Stocks,
-      as: "Stocks",
-      attributes: ['id', 'companyName', 'currentPrice', 'exchange', 'updatedAt'],
-    }],
+    include: [
+      {
+        model: Stocks,
+        as: "Stocks",
+        attributes: [
+          "id",
+          "companyName",
+          "currentPrice",
+          "exchange",
+          "updatedAt",
+        ],
+      },
+    ],
     group: ["ticker", "Stocks.ticker"],
   });
 
-  await Promise.all(stockList.map(async (stock) => {
-    console.log(`==========> fetching current price of : ${stock.ticker}`);
-    fetch(
-      "https://finnhub.io/api/v1/quote?symbol=" +
-        stock.ticker +
-        "&token=" +
-        FinnhubAPIKey
-    ).then(async (data) => {
-      await Stocks.update(
-        {
-          currentPrice: data.c,
-        },
-        { where: { ticker: stock.ticker } }
-      );
-      stock.currentPrice = data.c;
+  await Promise.all(
+    stockList.map(async (stock) => {
+      console.log(`==========> fetching current price of : ${stock.ticker}`);
+      // fetch(
+      //   "https://finnhub.io/api/v1/quote?symbol=" +
+      //     stock.ticker +
+      //     "&token=" +
+      //     FinnhubAPIKey
+      // ).then(async (data) => {
+      //   await Stocks.update(
+      //     {
+      //       currentPrice: data.c,
+      //     },
+      //     { where: { ticker: stock.ticker } }
+      //   );
+      //   stock.currentPrice = data.c;
+      // })
+      // .catch((err) => {
+      //   return res.json({error: err});
+      // });
     })
-    .catch((err) => {
-      return res.json({error: err});
-    });
-  }));
-  res.json(stockList)
+  );
+  res.json(stockList);
 });
 
 module.exports = router;
