a2a01bcca9876271a35e22ff38f3673e074a309b
Nari Roh <nari.rohh@gmail.com>
2022-01-23 22:14:18 -0500

    feat: implement getting stock infomation & update on current price

--- server/routes/portfolioRoute.js
+++ server/routes/portfolioRoute.js
@@ -5,81 +5,16 @@ const { validateToken } = require("../middlewares/AuthMiddleware");
 const Sequelize = require("sequelize");
 
 const fetch = require("node-fetch");
+const { response } = require("express");
 require("dotenv").config();
 
 const FinnhubAPIKey = process.env.FINNHUB_API_KEY;
-const FinnhubSandboxAPIKey = process.env.FINNHUB_SANDBOX_API_KEY;
-const AlphaVantageAPIKey = process.env.ALPHA_VANTAGE_API_KEY;
 
-// const UpdateQuote = require("../helpers/UpdateQuote")
-
-// router.get("/", validateToken, async (req, res) => {
-//   const user = await Users.findByPk(req.user.id);
-
-//   if (!user) {
-//     res.json({ error: "user dosen't exist" });
-//   } else {
-//     res.json(user);
-//   }
-// });
-
-// const stockList = await StockTransactions.findAll({
-//   where: { UserId: id},
-//   include: [{
-//     model: Stocks,
-//     as: "Stocks",
-//   }]
-// });
-// return {
-//   ticker: transaction.ticker,
-//   quantity: transaction.quantity,
-//   // sharePrice: transaction.sharePrice,
-// };
-
-// let buyList = buyTransaction.map(({ ticker, quantity }) => {
-//   return { ticker, quantity };
-// });
-
-// let buyList = buyTransaction.filter(function ({ ticker }) {
-//   return !this.has(ticker) && this.add(ticker);
-// }, new Set);
-
-// const stockList = await StockTransactions.findAll({
-//   where: { UserId },
-//   attributes: [
-//     "ticker",
-//     [
-//       Sequelize.fn(
-//         "SUM",
-//         Sequelize.literal("CASE type WHEN 'buy' THEN quantity else 0 end")
-//       ),
-//       "buyTotal",
-//     ],
-//     [
-//       Sequelize.fn(
-//         "SUM",
-//         Sequelize.literal("CASE type WHEN 'sell' THEN quantity else 0 end")
-//       ),
-//       "sellTotal",
-//     ],
-//     // [Sequelize.literal('"buyTotal" - "sellTotal"'), 'totalCounts']
-//   ],
-//   group: ["ticker"],
-// });
-
-/* Data needed: 
-    find type='buy'
-    StockTransactions: ticker, quantity, sharePrice or total StockId
-    Stocks: id?, companyName, currentPrice, exchange, sector?
-    
-    3. create a stockList then send it to client 
-    - new table to track stock quantity by stockId
-  */
 router.get("/stockList", validateToken, async (req, res) => {
   const { id: UserId } = req.user;
 
   // 1. get stocks and its quantities that user owns
-  let stockList = await StockTransactions.findAll({
+  const stockList = await StockTransactions.findAll({
     where: { UserId },
     attributes: [
       "ticker",
@@ -90,57 +25,35 @@ router.get("/stockList", validateToken, async (req, res) => {
         `totalCounts`,
       ],
     ],
-    group: ["ticker"],
+    include: [{
+      model: Stocks,
+      as: "Stocks",
+      attributes: ['id', 'companyName', 'currentPrice', 'exchange', 'updatedAt'],
+    }],
+    group: ["ticker", "Stocks.ticker"],
   });
 
-  // 2. make api call to get current price
-
-  // const { ticker: stockTicker} = stockList;
-  // console.log(ticker)
-
-  stockList.map(async ({ ticker }) => {
-    const stockInfo = await Stocks.findOne({
-      where: { ticker },
-      attributes: [
-        "ticker",
-        "companyName",
-        "currentPrice",
-        "exchange",
-        "updatedAt",
-      ],
-    }).then((stock) => {
-      const BASE_DATE = new Date(); // set it today
-
-      // if current price hasn't updated today (due to api call limit..)
-      // if (new Date(stock.updatedAt).getDay() !== BASE_DATE.getDay()) {
-        // fetch(
-        //   "https://finnhub.io/api/v1/quote?symbol=" +
-        //     stock.ticker +
-        //     "&token=" +
-        //     FinnhubAPIKey
-        // )
-        //   .then(async (data) => {
-        //     await Stocks.update(
-        //       {
-        //         currentPrice: data.c,
-        //       },
-        //       { where: { ticker } }
-        //     );
-        //     // console.log(data);
-        //     stock.currentPrice = data.c;
-        //   })
-        //   .catch((err) => {
-        //     res.json({ error: err });
-        //   });
-      // }
-      // console.log(stock)
-      console.log('from stock')
+  await Promise.all(stockList.map(async (stock) => {
+    console.log(`==========> fetching current price of : ${stock.ticker}`);
+    fetch(
+      "https://finnhub.io/api/v1/quote?symbol=" +
+        stock.ticker +
+        "&token=" +
+        FinnhubAPIKey
+    ).then(async (data) => {
+      await Stocks.update(
+        {
+          currentPrice: data.c,
+        },
+        { where: { ticker: stock.ticker } }
+      );
+      stock.currentPrice = data.c;
+    })
+    .catch((err) => {
+      return res.json({error: err});
     });
-    console.log('from stockList')
-
-  });
-
-  res.json(stockList);
+  }));
+  res.json(stockList)
 });
 
 module.exports = router;
