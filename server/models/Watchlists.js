de30c4b3bf21b516f1178990afaf11df8eb3c8e7
Nari Roh <nari.rohh@gmail.com>
2022-01-24 09:50:04 -0500

    feat: add relation to Stocks

--- server/models/Watchlists.js
+++ server/models/Watchlists.js
@@ -1,6 +1,19 @@
 module.exports = (sequelize, DataTypes) => {
-  const Watchlists = sequelize.define("Watchlists", {}, {
-    tableName: "watchlists",
-  });
+  const Watchlists = sequelize.define(
+    "Watchlists",
+    {},
+    {
+      tableName: "watchlists",
+    }
+  );
+
+  Watchlists.associate = (models) => {
+    Watchlists.belongsTo(models.Stocks, {
+      foreignKey: "StockId",
+      as: "Stocks",
+      onDelete: "CASCADE",
+    });
+  };
+
   return Watchlists;
 };
